3adda92d029e1839372029c70071ae9fde8b58da
kmunge <orokomunge@gmail.com>
2019-05-06 17:40:08 +0300

    creating eventlisteners on user activity

---
+++ src/app/user.ts
@@ -0,0 +1,5 @@
+export class User {
+    
+    constructor(public created_at:string, public login:string,public avatar_url: any,public name: string,public email: string,public public_repos:string,public bio:string,public location:string,public following: any,public followers: any,public html_url: any) {
+    }
+}
