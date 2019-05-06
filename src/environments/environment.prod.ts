0d7b2601dbd5dc24c1fa842afc7d9dfebdfb27b2
kmunge <orokomunge@gmail.com>
2019-05-06 18:54:31 +0300

    removing dist file from gitignore

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl:'https://api.github.com/users/',
+  accessToken:'?access_token=222b9b7199a7fbd038d2b0e895d576eadcce1232',
+  repos:'/repos'
 };
+
 
 
 
