a42e58c63bdc390bf998003fa521fc4e233b5f84
kmunge <orokomunge@gmail.com>
2019-05-06 17:46:45 +0300

    modifying to add api token

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: "https://api.github.com/users/",
+  accessToken: "?access_token=222b9b7199a7fbd038d2b0e895d576eadcce1232",
+  repos: "/repos"
 };
 
 /*
