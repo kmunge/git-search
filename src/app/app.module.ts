80cdaf430bc5f6b49fd52211843dad8c7ca0e559
kmunge <orokomunge@gmail.com>
2019-05-06 17:49:58 +0300

    importing modules to be required by program

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,42 @@
+import { RoutingModule } from './routing/routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpModule } from '@ngx-progressbar/http';
+import { RouterModule, Routes } from '@angular/router';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GithubComponent } from './github/github.component';
+import { UserComponent } from './user/user.component';
+import { RepositoryComponent } from './repository/repository.component';
+import { UserDetailsService } from './github-http/user-details.service';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GithubComponent,
+    UserComponent,
+    RepositoryComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    NgProgressModule,
+    NgProgressHttpModule,
+    RoutingModule
   ],
-  providers: [],
+  providers: [UserDetailsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
